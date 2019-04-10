6a35ee926ab011cffd49aaee04551b36c2fb1ce0
alvinjtang <alvintangdesigns@gmail.com>
2019-04-09 21:51:46 -0400

    f/creates server router file

---
+++ server/api/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+module.exports = router;
+
+router.use('`/users', require('./users'));
+router.use('/collections', require('./collections'));
+router.use('/cards', require('./cards'));
+
+router.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
